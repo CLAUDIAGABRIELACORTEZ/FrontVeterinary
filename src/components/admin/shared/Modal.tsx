0e03388313768a22622db346917bc6cd576a711f
Claudia <cortezgabriela693@gmail.com>
2024-11-22 05:26:29 -0400

    add modal

--- src/components/admin/shared/Modal.tsx
+++ src/components/admin/shared/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Pencil } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { RenderModalProps } from '@/types/index.types';
